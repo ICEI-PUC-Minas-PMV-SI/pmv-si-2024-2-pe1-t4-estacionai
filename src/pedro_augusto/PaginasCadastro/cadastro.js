976a454f0a6c3c247b402453f9d5338ef8e5daa3
Pedro Augusto Alves Oliveira Calazans <83885811+calazans17@users.noreply.github.com>
2024-11-05 20:36:31 -0300

    Correções

--- src/pedro_augusto/PaginasCadastro/cadastro.js
+++ src/pedro_augusto/PaginasCadastro/cadastro.js
@@ -1,3 +1,4 @@
+
 //Função para gerar numero aleatorio para o ID
 function generateNumericId() {
     return Math.floor(Math.random() * 999) + 1;
@@ -6,16 +7,18 @@ function generateNumericId() {
 
 //FUNÇÃO QUE O ENVIA OS DADOS PARA O JSON
 function submitlogin(){
-    alert('a função foi')
+  //  
 //Capturando valor do HTML
     const id = generateNumericId();
     const nome_usuario = document.getElementById("usuario").value;
     const senha = document.getElementById("usuario").value;
     const IsReserva = document.getElementById("usuario-reservar-vagas").value;
     const IsDisponibiliza = document.getElementById("usuario-disponibilizar-vagas").value;
-  
+    
+    console.log("segue os dados " + id,nome_usuario,senha,IsReserva,IsDisponibiliza)
+
 //Criando um objeto com os dados
-    const usuario = {
+    const usuarios= {
         id:id,
         user:nome_usuario,
         senha:senha,
@@ -24,12 +27,12 @@ function submitlogin(){
     };
 
 
-    fetch('.json/users.json',{
+    fetch('http://localhost:3000/usuarios',{
         method:'POST',
         headers:{
           'Content-Type': 'application/json'
         },
-        body:JSON.stringify(usuario)
+        body:JSON.stringify(usuarios)
     })
 
     .then(Response =>Response.json())
