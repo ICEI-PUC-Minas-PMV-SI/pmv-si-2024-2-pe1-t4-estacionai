50e1eda5df4a4c4a27172e05c84108075db7ce89
Pedro Augusto Alves Oliveira Calazans <83885811+calazans17@users.noreply.github.com>
2024-11-11 21:21:53 -0300

    Post usuarios

--- src/pedro_augusto/PaginasCadastro/cadastro.js
+++ src/pedro_augusto/PaginasCadastro/cadastro.js
@@ -1,31 +1,49 @@
 
-//Função para gerar numero aleatorio para o ID
-function generateNumericId() {
-    return Math.floor(Math.random() * 999) + 1;
-}
 
- 
+ const URL = 'https://estacionai-bd.onrender.com/usuarios'
 
 
 //FUNÇÃO QUE O ENVIA OS DADOS PARA O JSON
-function submitlogin(){
+async function submitlogin(){
     // Capturando valor do HTML
-    const id = generateNumericId();
+    
+    const email = document.getElementById("email").value
     const nome_usuario = document.getElementById("usuario").value;
     const senha = document.getElementById("senha").value;  // Corrigido
     const IsReserva = document.getElementById("usuario-reservar-vagas").value;
     const IsDisponibiliza = document.getElementById("usuario-disponibilizar-vagas").value;
     
-    console.log("segue os dados:", id, nome_usuario, senha, IsReserva, IsDisponibiliza);
+    console.log("Print dados -- ", nome_usuario, senha, IsReserva, IsDisponibiliza,email);
 
     // Criando um objeto com os dados
-    const usuarios = {
-        id: id,
+    const usuario = {
+        
         user: nome_usuario,
         senha: senha,
         IsDisponibiliza: IsDisponibiliza,
-        IsReserva: IsReserva
+        IsReserva: IsReserva,
+        email:email
     };
 
+    const config = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(usuario)
+      }
+      
+      try {
+        const retorno = await fetch(URL, config);    
 
+        if (retorno.ok) {
+          console.log("DEU CERTO");
+        } else {
+          console.log("Deu errado", retorno.status, retorno.statusText);
+        }
+      } catch (error) {
+        console.error("Erro ao fazer a requisição:1", error);
+      }
+  
+   
 }
