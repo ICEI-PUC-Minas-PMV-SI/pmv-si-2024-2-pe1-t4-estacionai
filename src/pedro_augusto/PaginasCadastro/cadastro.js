0a684f05edc82999ae3e963f1eeddbb81fbeaf44
Pedro Augusto Alves Oliveira Calazans <83885811+calazans17@users.noreply.github.com>
2024-11-12 21:06:27 -0300

    ATT

--- src/pedro_augusto/PaginasCadastro/cadastro.js
+++ src/pedro_augusto/PaginasCadastro/cadastro.js
@@ -2,18 +2,28 @@
 
  const URL = 'https://estacionai-bd.onrender.com/usuarios'
 
+ function ValidarEmail(email){
+  const padrao = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+  return padrao.test(email);
+ }
+    const email = document.getElementById("email").value
+    const nome_usuario = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;  // Corrigido
+    const verificarSenha = document.getElementById("senha_confirm").value
+    const IsReserva = document.getElementById("usuario-reservar-vagas").value;
+    const IsDisponibiliza = document.getElementById("usuario-disponibilizar-vagas").value;
+
+ reservaOn(set){
+  IsReserva = true
+ }
 
 //FUNÇÃO QUE O ENVIA OS DADOS PARA O JSON
 async function submitlogin(){
     // Capturando valor do HTML
     
-    const email = document.getElementById("email").value
-    const nome_usuario = document.getElementById("usuario").value;
-    const senha = document.getElementById("senha").value;  // Corrigido
-    const IsReserva = document.getElementById("usuario-reservar-vagas").value;
-    const IsDisponibiliza = document.getElementById("usuario-disponibilizar-vagas").value;
     
-    console.log("Print dados -- ", nome_usuario, senha, IsReserva, IsDisponibiliza,email);
+    
+    console.log("Print dados -- ", nome_usuario, senha, IsReserva, IsDisponibiliza,email," S",verificarSenha);
 
     // Criando um objeto com os dados
     const usuario = {
@@ -24,26 +34,44 @@ async function submitlogin(){
         IsReserva: IsReserva,
         email:email
     };
+    //Validação do tipo de cadastro
+    if(IsDisponibiliza==IsReserva){
+      alert("Campos preenchidos indevidamente")
+      return error
+    }
 
-    const config = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(usuario)
-      }
-      
-      try {
-        const retorno = await fetch(URL, config);    
-
-        if (retorno.ok) {
-          console.log("DEU CERTO");
-        } else {
-          console.log("Deu errado", retorno.status, retorno.statusText);
-        }
-      } catch (error) {
-        console.error("Erro ao fazer a requisição:1", error);
-      }
+    //Validação do e-mail
+    if(!ValidarEmail(email)){
+      alert("Por favor insira um e-mail valido")
+      return error
+    }
   
+    //VERIFICADO DA COMPATIBILIDADE DE SENHAS
+      if(senha==verificarSenha ){
+        //UP DE DADOS DE DADOS NO BANCO 
+        const config = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(usuario)
+        }
+        
+        try {
+          const retorno = await fetch(URL, config);    
+  
+          if (retorno.ok) {
+            console.log("DEU CERTO");
+          } else {
+            console.log("Deu errado", retorno.status, retorno.statusText);
+          }
+        } catch (error) {
+          console.error("Erro ao fazer a requisição:1", error);
+        }
+    
+      }
+      else{
+        alert("As senhas devem ser iguais")
+      }
    
 }
