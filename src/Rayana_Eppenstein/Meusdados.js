94dc9d8f13a99122b881e1a689ee23aeb9a45aab
Raquel Ribeiro <raquel.ribeiro.600492@sga.pucminas.br>
2024-11-28 10:53:26 -0300

    alterações

--- src/Rayana_Eppenstein/Meusdados.js
+++ src/Rayana_Eppenstein/Meusdados.js
@@ -2,7 +2,7 @@ const API_URL = "https://estacionai-bd.onrender.com/usuarios";
 
 // Redireciona para a página históricovagas
 document.querySelector(".btn-historico").addEventListener("click", function() {
-    window.location.href = "Raquel Ribeiro (Histórico de Vagas)/historicovagas.html"; 
+    window.location.href = "RaquelRibeiroHistoricodeVagas/historicovagas.html"; 
 });
 
 
